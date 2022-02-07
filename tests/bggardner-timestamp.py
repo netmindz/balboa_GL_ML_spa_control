e645d00b6aeb04f2c46319e24ecb1456fb363f48
Will Tatam <will@netmindz.net>
2022-02-07 18:54:43 +0000

    Update bggardner-timestamp to record the time interval between messages, rather than just the time - easier to read

--- tests/bggardner-timestamp.py
+++ tests/bggardner-timestamp.py
@@ -2,8 +2,10 @@ import serial
 import time
 
 s = serial.Serial("/dev/ttyUSB0", baudrate=115200)
+last = time.monotonic()
 while True:
     b = s.read(1)
     if len(b) == 0:
         continue
-    print("{:02X}@{:f}".format(b[0], time.monotonic()))
+    print("{:02X}@{:f}".format(b[0], (time.monotonic() - last)))
+    last = time.monotonic();
