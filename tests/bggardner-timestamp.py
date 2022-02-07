372747c292f3fe1235e166e57e284852c8e0eb7f
Will Tatam <will@netmindz.net>
2022-02-07 18:44:38 +0000

    Add test script from bggardner and roughly 1 min of output logged

---
+++ tests/bggardner-timestamp.py
@@ -0,0 +1,9 @@
+import serial
+import time
+
+s = serial.Serial("/dev/ttyUSB0", baudrate=115200)
+while True:
+    b = s.read(1)
+    if len(b) == 0:
+        continue
+    print("{:02X}@{:f}".format(b[0], time.monotonic()))
