d86c51264fa7648f44bddc3839bc4c3b601cca66
Will Tatam <will@netmindz.net>
2022-03-11 20:10:08 +0000

    Tweaks

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -1,5 +1,12 @@
+#ifdef ESP32
 #include <WiFi.h>
+#else
+#include <ESP8266WiFi.h>
+#endif
 #include <ArduinoHA.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
 #include "wifi.h"
 // Create file with the following
@@ -14,12 +21,14 @@ const char passphrase[] = SECRET_PSK;   /* Replace with your WPA2 passphrase */
 #define BROKER_ADDR IPAddress(192,168,178,42)
 
 byte mac[] = {0x00, 0x10, 0xFA, 0x6E, 0x38, 0x4A};
+// WiFi.macAddress();
 unsigned long lastSentAt = millis();
 double lastValue = 0;
 
-WiFiClient client;
+WiFiClient clients[2];
+WiFiClient client = clients[1];
 HADevice device(mac, sizeof(mac));
-HAMqtt mqtt(client, device);
+HAMqtt mqtt(clients[0], device);
 HASensor temp("temp"); // "temp" is unique ID of the sensor. You should define your own ID.
 
 void onBeforeSwitchStateChanged(bool state, HASwitch* s)
@@ -54,6 +63,37 @@ void setup() {
   Serial.print(F("Connected with IP: "));
   Serial.println(WiFi.localIP());
 
+  ArduinoOTA.setHostname("hottub-sensor");
+
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+
+
   // set device's details (optional)
   device.setName("Hottub");
   device.setSoftwareVersion("0.0.1");
@@ -72,9 +112,10 @@ String result = "";
 double tempValue;
 void loop() {
   mqtt.loop();
+  ArduinoOTA.handle();
 
   if (!isConnected) {
-    Serial.println("Try to connect to hottub...");
+    Serial.print("Try to connect to hottub ... ");
     if (!client.connect("hottub", 7777)) {
       Serial.println("Connection failed.");
       Serial.println("Waiting 5 seconds before retrying...");
@@ -90,23 +131,22 @@ void loop() {
 
   if (isConnected && client.available() > 0) {
     size_t len = client.available();
-    //    Serial.printf("bytes avail = %u\n", len);
+//        Serial.printf("bytes avail = %u\n", len);
     uint8_t buf[len];
     client.read(buf, len);
     for (int i = 0; i < len; i++) {
       if (String(buf[i], HEX) == "fa") {
-        Serial.printf("HexString2ASCIIString message = %s\n", HexString2ASCIIString(result));
+//        Serial.printf("HexString2ASCIIString message = %s\n", HexString2ASCIIString(result));
         Serial.print("message = ");
         Serial.println(result);
 
         if(result.substring(0,4) == "fa14") {
           tempValue = (HexString2ASCIIString(result.substring(4,10)).toDouble() / 10);
           Serial.printf("temp = %f\n", tempValue);
-          Serial.println(tempValue);
         }
         
         result = ""; // clear buffer
-        Serial.println("\n");
+//        Serial.println("\n");
       }
       if (buf[i] < 0x10) {
         result += '0';
