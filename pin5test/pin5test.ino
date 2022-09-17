e788941306908227172e58228ddfdd3b6b9c0846
Will Tatam <will@netmindz.net>
2022-09-17 19:43:20 +0100

    Try and write a command back to the tub at the right point in the messages - just after a short FA

--- pin5test/pin5test.ino
+++ pin5test/pin5test.ino
@@ -27,7 +27,9 @@ void loop() {
     if(tmp.length() == 46) {
       String sendBuffer = "fb0603450e0009f6f6"; // toggle light
       Serial.println("Sending " + sendBuffer);
-      Serial2.write(sendBuffer.c_str());
+      byte byteArray[18] = {0};
+      hexCharacterStringToBytes(byteArray, sendBuffer.c_str());
+      Serial2.write(byteArray, sizeof(byteArray));
     }
   }
   i++;
@@ -48,4 +50,64 @@ void buildString(uint8_t buf[], size_t len) {
      }
     tmp += String(buf[i], HEX);
   }
+}
+
+void hexCharacterStringToBytes(byte *byteArray, const char *hexString)
+{
+  bool oddLength = strlen(hexString) & 1;
+
+  byte currentByte = 0;
+  byte byteIndex = 0;
+
+  for (byte charIndex = 0; charIndex < strlen(hexString); charIndex++)
+  {
+    bool oddCharIndex = charIndex & 1;
+
+    if (oddLength)
+    {
+      // If the length is odd
+      if (oddCharIndex)
+      {
+        // odd characters go in high nibble
+        currentByte = nibble(hexString[charIndex]) << 4;
+      }
+      else
+      {
+        // Even characters go into low nibble
+        currentByte |= nibble(hexString[charIndex]);
+        byteArray[byteIndex++] = currentByte;
+        currentByte = 0;
+      }
+    }
+    else
+    {
+      // If the length is even
+      if (!oddCharIndex)
+      {
+        // Odd characters go into the high nibble
+        currentByte = nibble(hexString[charIndex]) << 4;
+      }
+      else
+      {
+        // Odd characters go into low nibble
+        currentByte |= nibble(hexString[charIndex]);
+        byteArray[byteIndex++] = currentByte;
+        currentByte = 0;
+      }
+    }
+  }
+}
+
+byte nibble(char c)
+{
+  if (c >= '0' && c <= '9')
+    return c - '0';
+
+  if (c >= 'a' && c <= 'f')
+    return c - 'a' + 10;
+
+  if (c >= 'A' && c <= 'F')
+    return c - 'A' + 10;
+
+  return 0;  // Not a valid hexadecimal character
 }
