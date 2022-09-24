e9415ecc49ce98cd36049f69022d835b9af2c1e0
Will Tatam <will@netmindz.net>
2022-09-24 12:46:09 +0100

    Updates on trying to send data

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -598,9 +598,7 @@ void handlMessage() {
         telnetSend(result + " port:" + portIndex);
         sendBuffer = "fb0603450e0009f6f6";
         if(sendBuffer != "") {
-          Serial.println("Sending " + sendBuffer);
-          telnetSend("W: " + sendBuffer);
-          tub.write(sendBuffer.c_str());
+          writeToTub(sendBuffer);
 //          sendBuffer = "";
         }
       }
@@ -618,6 +616,75 @@ void handlMessage() {
       }
 
       result = ""; // clear buffer
+}
+
+void writeToTub(String value) {
+  byte byteArray[100] = {0};
+  // As when we read, we convert bytes to hex, i'm assuming we need to do the same to write?
+  hexCharacterStringToBytes(byteArray, value.c_str());
+  Serial.println("Sending " + value);
+  telnetSend("W: " + value);
+  tub.write(byteArray, sizeof(value.c_str()));
+}
+
+void hexCharacterStringToBytes(byte *byteArray, const char *hexString)
+{
+  bool oddLength = strlen(hexString) & 1;
+
+  byte currentByte = 0;
+  byte byteIndex = 0;
+
+  for (byte charIndex = 0; charIndex < strlen(hexString); charIndex++)
+  {
+    bool oddCharIndex = charIndex & 1;
+
+    if (oddLength)
+    {
+      // If the length is odd
+      if (oddCharIndex)
+      {
+        // odd characters go in high nibble
+        currentByte = nibble(hexString[charIndex]) << 4;
+      }
+      else
+      {
+        // Even characters go into low nibble
+        currentByte |= nibble(hexString[charIndex]);
+        byteArray[byteIndex++] = currentByte;
+        currentByte = 0;
+      }
+    }
+    else
+    {
+      // If the length is even
+      if (!oddCharIndex)
+      {
+        // Odd characters go into the high nibble
+        currentByte = nibble(hexString[charIndex]) << 4;
+      }
+      else
+      {
+        // Odd characters go into low nibble
+        currentByte |= nibble(hexString[charIndex]);
+        byteArray[byteIndex++] = currentByte;
+        currentByte = 0;
+      }
+    }
+  }
+}
+
+byte nibble(char c)
+{
+  if (c >= '0' && c <= '9')
+    return c - '0';
+
+  if (c >= 'a' && c <= 'f')
+    return c - 'a' + 10;
+
+  if (c >= 'A' && c <= 'F')
+    return c - 'A' + 10;
+
+  return 0;  // Not a valid hexadecimal character
 }
 
 String HexString2TimeString(String hexstring){
