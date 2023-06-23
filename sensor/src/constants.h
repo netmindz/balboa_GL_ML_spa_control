987b378a8fe03fb6d99e21a56102d6b64f4bb72d
Will Tatam <will@netmindz.net>
2023-06-23 18:31:05 +0100

    Add comment about bad JET commands

--- sensor/src/constants.h
+++ sensor/src/constants.h
@@ -4,8 +4,8 @@
 // #define COMMAND_TIME        "fb066666666603fc76"
 #define COMMAND_CHANGE_MODE "fb0603450e0004fb3c"
 // #define COMMAND_BLOWER      "fb06666666660af5f4"
-#define COMMAND_JET1        "fb06030a450e6f918"
-#define COMMAND_JET2        "fb06030a450e7f80a"
+#define COMMAND_JET1        "fb06030a450e6f918" // TODO: fix me - one char short
+#define COMMAND_JET2        "fb06030a450e7f80a" // TODO: fix me - one char short
 #define COMMAND_EMPTY       "fb0603450e0000ff74"
 #define COMMAND_LIGHT       "fb0603450e0009f6f6"
 
