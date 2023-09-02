bea7e0e04fe5f12a0059abd3859ae81bab1f8ff5
Will Tatam <will@netmindz.net>
2023-09-02 13:16:38 +0100

    Trying to fix missing class

--- components/BalboaGL/ESPBalboaGL.h
+++ components/BalboaGL/ESPBalboaGL.h
@@ -29,34 +29,34 @@ const int MINUTES_PER_DEGC = 45;
 
 using namespace esphome;
 
-class BalboaGL : public Component, public climate::Climate {
+class BalboaGL : public Component { // }, public climate::Climate {
  public:
   void setup() override {
     // This will be called by App.setup()
   }
-  void control(const ClimateCall &call) override {
-    if (call.get_mode().has_value()) {
-      // User requested mode change
-      ClimateMode mode = *call.get_mode();
-      // Send mode to hardware
-      // ...
+  // void control(const ClimateCall &call) override {
+  //   if (call.get_mode().has_value()) {
+  //     // User requested mode change
+  //     ClimateMode mode = *call.get_mode();
+  //     // Send mode to hardware
+  //     // ...
 
-      // Publish updated state
-      this->mode = mode;
-      this->publish_state();
-    }
-    if (call.get_target_temperature().has_value()) {
-      // User requested target temperature change
-      float temp = *call.get_target_temperature();
-      // Send target temp to climate
-      // ...
-    }
-  }
-  ClimateTraits traits() override {
-    // The capabilities of the climate device
-    auto traits = climate::ClimateTraits();
-    traits.set_supports_current_temperature(true);
-    traits.set_supported_modes({climate::CLIMATE_MODE_HEAT_COOL});
-    return traits;
-  }
+  //     // Publish updated state
+  //     this->mode = mode;
+  //     this->publish_state();
+  //   }
+  //   if (call.get_target_temperature().has_value()) {
+  //     // User requested target temperature change
+  //     float temp = *call.get_target_temperature();
+  //     // Send target temp to climate
+  //     // ...
+  //   }
+  // }
+  // ClimateTraits traits() override {
+  //   // The capabilities of the climate device
+  //   auto traits = climate::ClimateTraits();
+  //   traits.set_supports_current_temperature(true);
+  //   traits.set_supported_modes({climate::CLIMATE_MODE_HEAT_COOL});
+  //   return traits;
+  // }
 }
