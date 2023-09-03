e347ab19fa5446d0105b3273df436e17c4818a24
Will Tatam <will@netmindz.net>
2023-09-03 11:57:10 +0100

    remove dups

--- components/BalboaGL/ESPBalboaGL.h
+++ components/BalboaGL/ESPBalboaGL.h
@@ -10,25 +10,25 @@
 
 static const char* TAG = "BalboaGL"; // Logging tag
 
-// Perform measurements or read nameplate values on your tub to define the power [kW]
-// for each device in order to calculate tub power usage
-const float POWER_HEATER = 2.8;
-const float POWER_PUMP_CIRCULATION = 0.3;
-const float POWER_PUMP1_LOW = 0.31;
-const float POWER_PUMP1_HIGH = 1.3;
-const float POWER_PUMP2_LOW = 0.3;
-const float POWER_PUMP2_HIGH = 0.6;
+// // Perform measurements or read nameplate values on your tub to define the power [kW]
+// // for each device in order to calculate tub power usage
+// const float POWER_HEATER = 2.8;
+// const float POWER_PUMP_CIRCULATION = 0.3;
+// const float POWER_PUMP1_LOW = 0.31;
+// const float POWER_PUMP1_HIGH = 1.3;
+// const float POWER_PUMP2_LOW = 0.3;
+// const float POWER_PUMP2_HIGH = 0.6;
 
-// Tweak for your tub - would be nice to auto-learn in the future to allow for outside temp etc
-const int MINUTES_PER_DEGC = 45;
+// // Tweak for your tub - would be nice to auto-learn in the future to allow for outside temp etc
+// const int MINUTES_PER_DEGC = 45;
 
 const uint32_t POLL_INTERVAL_DEFAULT = 10000;
 
 #define tub Serial2
 #define RX_PIN 19
 #define TX_PIN 23
-#define RTS_PIN 22  // RS485 direction control, RequestToSend TX or RX, required for MAX485 board.
-#define PIN_5_PIN 18
+// #define RTS_PIN 22  // RS485 direction control, RequestToSend TX or RX, required for MAX485 board.
+// #define PIN_5_PIN 18
 #define LED_BUILTIN 2
 
 
