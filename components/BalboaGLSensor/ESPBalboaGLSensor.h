4852fd68528a9099d708b3a01441754f8f338b7e
Will Tatam <will@netmindz.net>
2023-09-24 20:43:11 +0100

    BalboaGLSensor

--- components/BalboaGLSensor/ESPBalboaGLSensor.h
+++ components/BalboaGLSensor/ESPBalboaGLSensor.h
@@ -9,10 +9,10 @@ namespace esphome {
   using namespace text_sensor;
 
   class BalboaGLSensor : public TextSensor, public Component {
-  public:
-    void setup() override;
-    void update() override;
-    TextSensor *status_sensor = new TextSensor();
+    public:
+      void setup();
+      void update();
+      TextSensor *status_sensor = new TextSensor();
 
   };
 
