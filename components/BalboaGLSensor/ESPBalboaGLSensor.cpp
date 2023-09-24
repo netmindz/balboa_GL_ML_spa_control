e57762d2fc950c08f2786ad7463d27e734ccad17
Will Tatam <will@netmindz.net>
2023-09-24 20:33:06 +0100

    Creating BalboaGLSensor

--- components/BalboaGLSensor/ESPBalboaGLSensor.cpp
+++ components/BalboaGLSensor/ESPBalboaGLSensor.cpp
@@ -2,12 +2,16 @@
 #include "ESPBalboaGLSensor.h"
 #include "balboaGL.h"
 
+namespace esphome {
+  namespace empty_text_sensor {
 
-BalboaGLSensor::BalboaGLSensor() : PollingComponent(15000) { }
+    BalboaGLSensor::BalboaGLSensor() : PollingComponent(15000) { }
 
-void BalboaGLSensor::setup()  {
-}
+    void BalboaGLSensor::setup()  {
+    }
 
-void BalboaGLSensor::update()  {
-  status_sensor->publish_state(status.state.c_str());
+    void BalboaGLSensor::update()  {
+      status_sensor->publish_state(status.state.c_str());
+    }
+  }
 }
--- components/BalboaGLSensor/ESPBalboaGLSensor.h
+++ components/BalboaGLSensor/ESPBalboaGLSensor.h
@@ -4,15 +4,17 @@
 #include "esphome/components/text_sensor/text_sensor.h"
 
 namespace esphome {
+  namespace empty_text_sensor {
 
-using namespace text_sensor;
+  using namespace text_sensor;
 
-class BalboaGLSensor : public TextSensor, public Component {
- public:
-  void setup() override;
-  void update() override;
-  TextSensor *status_sensor = new TextSensor();
+  class BalboaGLSensor : public TextSensor, public Component {
+  public:
+    void setup() override;
+    void update() override;
+    TextSensor *status_sensor = new TextSensor();
 
-};
+  };
 
-}  // namespace esphome
+  }
+}  
