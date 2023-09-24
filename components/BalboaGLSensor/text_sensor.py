f06d51a15b43919bbdd85b0d2ac422c344bc0496
Will Tatam <will@netmindz.net>
2023-09-24 21:37:28 +0100

    BalboaGLSensor - define multiple sensors

--- components/BalboaGLSensor/text_sensor.py
+++ components/BalboaGLSensor/text_sensor.py
@@ -3,6 +3,10 @@ import esphome.config_validation as cv
 from esphome.components import text_sensor
 from esphome.const import CONF_ID
 
+CONF_STATE = "state"
+CONF_RAW = "raw"
+
+
 balboa_sensor_ns = cg.esphome_ns.namespace('balboa_sensor')
 StateInfo = balboa_sensor_ns.class_('BalboaGLStateSensor', text_sensor.TextSensor, cg.Component)
 RawInfo = balboa_sensor_ns.class_('BalboaGLRawSensor', text_sensor.TextSensor, cg.Component)
@@ -13,16 +17,15 @@ RawInfo = balboa_sensor_ns.class_('BalboaGLRawSensor', text_sensor.TextSensor, c
 
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.Optional("state"): text_sensor.TEXT_SENSOR_SCHEMA.extend({
+    cv.Optional(CONF_STATE): text_sensor.TEXT_SENSOR_SCHEMA.extend({
         cv.GenerateID(): cv.declare_id(StateInfo),
     }),
-    cv.Optional("raw"): text_sensor.TEXT_SENSOR_SCHEMA.extend({
+    cv.Optional(CONF_RAW): text_sensor.TEXT_SENSOR_SCHEMA.extend({
         cv.GenerateID(): cv.declare_id(RawInfo),
     }),
 })
 
 def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    yield text_sensor.register_text_sensor(var, config)
-    yield cg.register_component(var, config)
+    yield setup_conf(config, CONF_STATE)
+    yield setup_conf(config, CONF_RAW)
     
