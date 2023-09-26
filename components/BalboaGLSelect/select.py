407d53933d4e021c8cdf46772658dad273da7c91
Will Tatam <will@netmindz.net>
2023-09-26 20:46:52 +0100

    Add select for pump1

--- components/BalboaGLSelect/select.py
+++ components/BalboaGLSelect/select.py
@@ -6,11 +6,11 @@ from esphome.const import CONF_ID
 select_ns = cg.esphome_ns.namespace('balboa_select')
 Pump1Select = select_ns.class_('BalboaGLPump1Select', select.Select, cg.Component)
 
-CONFIG_SCHEMA = select.SWITCH_SCHEMA.extend({
+CONFIG_SCHEMA = select.SELECT_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(Pump1Select)
 }).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
-    yield select.tch.register_switch(var, config)
+    yield select.register_select(var, config)
