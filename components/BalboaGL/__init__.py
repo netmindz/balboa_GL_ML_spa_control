99bd64d3ceb6a799160f1226ba4a4906bcf408ae
Will Tatam <will@netmindz.net>
2023-10-17 22:01:28 +0100

    Auto load components

--- components/BalboaGL/__init__.py
+++ components/BalboaGL/__init__.py
@@ -14,7 +14,7 @@ from esphome.const import (
 )
 from esphome.core import CORE, coroutine
 
-AUTO_LOAD = ["climate"]
+AUTO_LOAD = ["climate","switch","select","sensor"]
 
 CONF_SUPPORTS = "supports"
 DEFAULT_CLIMATE_MODES = ["HEAT","AUTO"]
