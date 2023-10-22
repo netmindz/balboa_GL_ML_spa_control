cab31c33ab2b9bee07c81bd10c51218d002d66c1
Will Tatam <will@netmindz.net>
2023-10-22 23:17:00 +0100

    text sesnor

--- components/BalboaGL/text_sensor/BalboaGLSesnor.h
+++ components/BalboaGL/text_sensor/BalboaGLSesnor.h
@@ -8,13 +8,13 @@
 namespace esphome {
     namespace balboa_sensor {
 
-    class BalboaGLStateSensor : public Component, public text_sensor::TextSensor {
+    class BalboaGLStateSensor : public PollingComponent, public text_sensor::TextSensor {
             public:
             void setSpa(balboaGL* spa) {
                 this->spa = spa;
             }
             void update() override {
-                publish_state(status.state.c_ctr());
+                publish_state(status.state.c_str());
             }
             private:
             balboaGL* spa;
