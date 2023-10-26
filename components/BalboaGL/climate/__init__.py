f2bfca25f902222d5c2ddfd54341a28f2dc1cc8c
Will Tatam <will@netmindz.net>
2023-10-26 19:03:41 +0100

    Remove unused reference to UART

--- components/BalboaGL/climate/__init__.py
+++ components/BalboaGL/climate/__init__.py
@@ -1,7 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate
-from esphome.components.logger import HARDWARE_UART_TO_SERIAL
 from esphome.const import (
     CONF_ID,
     CONF_UPDATE_INTERVAL,
