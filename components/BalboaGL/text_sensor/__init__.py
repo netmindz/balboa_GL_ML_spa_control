b83768a5988109ae2231fb817a2e891f5c7b21a6
Will Tatam <will@netmindz.net>
2023-10-27 19:39:32 +0100

    State sensor

--- components/BalboaGL/text_sensor/__init__.py
+++ components/BalboaGL/text_sensor/__init__.py
@@ -19,29 +19,21 @@ StateInfo = cg.esphome_ns.class_('BalboaGLStateSensor', text_sensor.TextSensor, 
 # RawInfo = balboagl_ns.class_('BalboaGLRawSensor', sensor.Sensor, cg.Component)
 # LCDInfo = balboagl_ns.class_('BalboaGLLCDSensor', sensor.Sensor, cg.Component)
 
-CONFIG_SCHEMA = text_sensor.TEXT_SENSOR_SCHEMA.extend(
-    {
+CONFIG_SCHEMA = (
+    text_sensor.text_sensor_schema(StateInfo)
+    .extend(
+        {
+            cv.GenerateID(CONF_BALBOA_ID): cv.use_id(BalboaGL),
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
+)
 
-    cv.GenerateID(): cv.declare_id(StateInfo),
-    cv.GenerateID(CONF_BALBOA_ID): cv.use_id(BalboaGL),
-        
-    # cv.Optional(CONF_RAW): text_sensor.TEXT_SENSOR_SCHEMA.extend({
-    #     cv.GenerateID(): cv.declare_id(RawInfo),
-    # }),
-    # cv.Optional(CONF_LCD): text_sensor.TEXT_SENSOR_SCHEMA.extend({
-    #     cv.GenerateID(): cv.declare_id(LCDInfo),
-    # }),
-}).extend(cv.COMPONENT_SCHEMA)
-
-@coroutine
-def setup_conf(config, key):
+async def setup_conf(config, key):
     if key in config:
         conf = config[key]
-        var = cg.new_Pvariable(conf[CONF_ID])
-        yield cg.register_component(var, conf)
-        yield text_sensor.register_text_sensor(var, conf)
+        var = await text_sensor.new_text_sensor(conf)
+        await cg.register_component(var, conf)
 
-def to_code(config):
-    yield setup_conf(config, CONF_STATE)
-    # yield setup_conf(config, CONF_RAW)
-    # yield setup_conf(config, CONF_LCD)
+async def to_code(config):
+    await setup_conf(config, CONF_STATE)
