5fe2da710bd66f60a2c23de0b8bb430eed7f73eb
Will Tatam <will@netmindz.net>
2023-11-02 19:03:05 +0000

    buttons

--- components/BalboaGL/button/BalboaGLButton.h
+++ components/BalboaGL/button/BalboaGLButton.h
@@ -7,17 +7,19 @@ using namespace esphome;
 class UpButton : public PollingComponent, public button::Button {
  public:
   void press_action() {}
+  void update() {}
 
 };
 
 class DownButton : public PollingComponent, public button::Button {
  public:
   void press_action() {}
-
+  void update() {}
 };
 
 class ModeButton : public PollingComponent, public button::Button {
  public:
   void press_action() {}
+  void update() {}
 
 };
