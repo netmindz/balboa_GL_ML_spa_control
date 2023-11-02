24906bb427db39d27ccd1d53c3f7ab6fbda2ca75
Will Tatam <will@netmindz.net>
2023-11-02 14:34:50 +0000

    starting value and remove unqiue_id

--- components/BalboaGL/text_sensor/BalboaGLTextSesnor.h
+++ components/BalboaGL/text_sensor/BalboaGLTextSesnor.h
@@ -12,13 +12,16 @@ class BalboaGLStateSensor : public PollingComponent, public text_sensor::TextSen
     // void setSpa(balboaGL* spa) {
     //     this->spa = spa;
     // }
+    void setup() override {
+        this->last_value = "Awaiting data";
+    }
     void update() override {
         if(status.state != this->last_value) {
             this->last_value = status.state;
             publish_state(last_value.c_str());
         }
     }
-    std::string unique_id() override { return "state"; }
+    // std::string unique_id() override { return "state"; }
     private:
       String last_value;
         
@@ -29,13 +32,16 @@ class BalboaGLRawSensor : public PollingComponent, public text_sensor::TextSenso
     // void setSpa(balboaGL* spa) {
     //     this->spa = spa;
     // }
+    void setup() override {
+        this->last_value = "no data";
+    }
     void update() override {
         if(status.rawData != this->last_value) {
             this->last_value = status.rawData;
             publish_state(last_value.c_str());
         }
     }
-    std::string unique_id() override { return "raw"; }
+    // std::string unique_id() override { return "raw"; }
     private:
       String last_value;
         
@@ -46,13 +52,16 @@ class BalboaGLLCDSensor : public PollingComponent, public text_sensor::TextSenso
     // void setSpa(balboaGL* spa) {
     //     this->spa = spa;
     // }
+    void setup() override {
+        this->last_value = "unknown";
+    }
     void update() override {
         if(String(status.lcd) != this->last_value) {
             this->last_value = String(status.lcd);
             publish_state(last_value.c_str());
         }
     }
-    std::string unique_id() override { return "lcd"; }
+    // std::string unique_id() override { return "lcd"; }
     private:
       String last_value;
         
