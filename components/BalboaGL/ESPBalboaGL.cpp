53bba06efe5dffd998ce76039457c883b53364c1
Will Tatam <will@netmindz.net>
2023-11-11 17:45:32 +0000

    Revert "Try invert"
    This reverts commit e2a93cfadfcc52a4651cc6b8bd1f9807bb8df862.

--- components/BalboaGL/ESPBalboaGL.cpp
+++ components/BalboaGL/ESPBalboaGL.cpp
@@ -95,8 +95,7 @@ void BalboaGL::setup() {
     ESP_LOGCONFIG(TAG, "Initialize new balboaGL object.");
 
     ESP_LOGI(TAG, "Serial begin rx,tx = %u,%u", this->rx_pin, this->tx_pin);
-    bool invert = true;
-    hw_serial_->begin(115200, SERIAL_8N1, rx_pin, tx_pin, invert);
+    hw_serial_->begin(115200, SERIAL_8N1, rx_pin, tx_pin);
     this->spa = new balboaGL(hw_serial_, rts_pin, panel_select_pin, ESP_LOG_VERBOSE);
     this->spa->attachPanelInterrupt();
     if(delay_time > -1) this->spa->set_delay_time(delay_time);
